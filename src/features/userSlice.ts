afbde9189346770aa97ca8cf4a638185629c4491
aiichiro <aiichirookuda@gmail.com>
2021-06-16 18:29:42 +0900

    create useSlice

---
+++ src/features/userSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from '../app/store';
+
+export const userSlice = createSlice({
+  name: 'name',
+  initialState: {
+    user: { uid: '', photoUrl: '', displayName: '' },
+  },
+
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = { uid: '', photoUrl: '', displayName: '' };
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+
+export const selectCount = (state: RootState) => state.user.user;
+
+export default userSlice.reducer;
