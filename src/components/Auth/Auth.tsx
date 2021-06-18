3c9f29377fa54b1d988362b732b912fec3ba25c3
aiichiro <aiichirookuda@gmail.com>
2021-06-18 18:08:51 +0900

    create reset password function

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -23,7 +23,17 @@ import CameraIcon from '@material-ui/icons/Camera';
 import EmailIcon from '@material-ui/icons/Email';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import { Email } from '@material-ui/icons';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -56,6 +66,15 @@ const useStyles = makeStyles((theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
+  modal: {
+    outline: 'none',
+    position: 'absolute',
+    width: 400,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(10),
+  },
 }));
 
 export const Auth: React.FC = () => {
@@ -66,6 +85,21 @@ export const Auth: React.FC = () => {
   const [username, setUsername] = useState<string>('');
   const [avatarImage, setAvatarImage] = useState<File | null>(null);
   const [isLogin, setIsLogin] = useState<boolean>(true);
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const [resetEmail, setResetEmail] = useState<string>('');
+
+  const sendResetEmail = async (e: React.MouseEvent<HTMLElement>) => {
+    await auth
+      .sendPasswordResetEmail(resetEmail)
+      .then(() => {
+        setOpenModal(false);
+        setResetEmail('');
+      })
+      .catch((err) => {
+        alert(err.message);
+        setResetEmail('');
+      });
+  };
 
   const onChangeImageHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files![0]) {
@@ -224,7 +258,12 @@ export const Auth: React.FC = () => {
             </Button>
             <Grid container>
               <Grid item xs>
-                <span className={Styles.login_reset}>Forgot password ?</span>
+                <span
+                  className={Styles.login_reset}
+                  onClick={() => setOpenModal(true)}
+                >
+                  Forgot password ?
+                </span>
               </Grid>
               <Grid item>
                 <span
@@ -239,12 +278,34 @@ export const Auth: React.FC = () => {
               fullWidth
               variant="contained"
               color="primary"
+              startIcon={<CameraIcon />}
               className={classes.submit}
               onClick={signInGoogle}
             >
               SignIn with Google
             </Button>
           </form>
+          <Modal open={openModal} onClose={() => setOpenModal(false)}>
+            <div style={getModalStyle()} className={classes.modal}>
+              <div className={Styles.login_modal}>
+                <TextField
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  type="email"
+                  name="email"
+                  label="Reset E-mail"
+                  value={resetEmail}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setResetEmail(e.target.value)
+                  }
+                />
+                <IconButton onClick={sendResetEmail}>
+                  <SendIcon />
+                </IconButton>
+              </div>
+            </div>
+          </Modal>
         </div>
       </Grid>
     </Grid>
