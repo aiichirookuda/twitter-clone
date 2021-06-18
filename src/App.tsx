f1308f1089589678470abd108b8739360f516ae3
aiichiro <aiichirookuda@gmail.com>
2021-06-18 16:30:53 +0900

    fix: photo => photoUrl

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
         dispatch(
           login({
             uid: authUser.uid,
-            photo: authUser.photoURL,
+            photoUrl: authUser.photoURL,
             displayName: authUser.displayName,
           })
         );
